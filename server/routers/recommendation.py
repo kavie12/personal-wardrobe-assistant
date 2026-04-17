6ec070d119146b44b5065603c2646bd4dc3a3119
Kaveesha Dissanayake <kaveeshasandaruwan1111@gmail.com>
2026-04-17 20:33:19 +0530

    removed rule-based recommendation system

--- server/routers/recommendation.py
+++ server/routers/recommendation.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from dependencies.auth import get_current_uid
-from services import recommendation_rule_based as recommendation
+from services import recommendation
 from pydantic import BaseModel
 from typing import Dict, Any
 
